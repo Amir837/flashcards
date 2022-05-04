4edda3647b9c6d21a093e58a9def259cbd7968f0
Amir837 <amir.maksyutov@outlook.com>
2022-05-03 17:21:23 -0700

    change

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,7 @@ import PGNp from './PGNp';
 
 ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter>
-      <Routes>
-        <Route path = "/" element = { <App /> } />
-        <Route path = "/Board" element = { <Board /> } />
-        <Route path = "/PGNp" element = { <PGNp /> } />
-        <Route path = "*" element = { <h1>404</h1> } />
-      </Routes>
-      </BrowserRouter>
+      <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
