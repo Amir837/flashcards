6b515a0e0a7fc4ed7418f0b90852cfccd3a1f482
Amir837 <amir.maksyutov@outlook.com>
2022-05-03 17:30:46 -0700

    first version

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Board from './Board';
 import App from './App';
-import PGNp from './PGNp';
 
 
 ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = { <App /> } />
+      </Routes>
+      </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
